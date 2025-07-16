9d6c771bb56c36933e7b79d3018635e33e38317d
suriyamuthukumarasamy <suriyamuthukumarasamy@gmail.com>
2025-07-16 17:45:12 +0530

    cors change

--- index.js
+++ index.js
@@ -15,25 +15,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 //  CORS Setup
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      const allowedOrigins = [
-        'http://localhost:3000',
-        'https://health-first-ctoy.vercel.app'
-      ];
-
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error('❌ Not allowed by CORS'));
-      }
-    },
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true,
-  })
-);
+app.use(cors({
+  origin: '*', // allows all domains
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
 
 
 //  Root Route
